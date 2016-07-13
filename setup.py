d91f57ecdbd499a9495c0f6745f4c95815c836e1
freddylimpens <freddy.limpens@gmail.com>
2016-07-13 13:25:08 +0200

    versino

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def package_data(pkg, root):
 
 setup(
     name='xblock-cnvideo',
-    version='0.1.4',
+    version='0.1.4-alpha',
     description='plugin pour importer des videos dans un cours',
     #py_modules=['cnvideo'],
     packages=['cnvideo'],
